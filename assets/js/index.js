f695055d2753ae602523012fdae3d975223ec093
shayene <shaylcm62@gmail>
2022-07-19 16:23:37 -0300

    ajuste nas imagens do site

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,8 +4,15 @@ function menu_mobile()
 
   if(document.getElementById("menus").style.display == 'block'){
 	   document.getElementById("menus").style.display = 'none';
+     document.getElementById("botaomenux").style.display = 'none';
+     document.getElementById("botaomenu").style.display = 'block';
+
+
   }else{
-	document.getElementById("menus").style.display = 'block';
+    
+     document.getElementById("botaomenu").style.display = 'none';
+     document.getElementById("botaomenux").style.display = 'block';
+	   document.getElementById("menus").style.display = 'block';
   };
 
   // document.getElementById("mobile").style.backgroundColor = " #141F49"
@@ -15,5 +22,6 @@ function menu_mobile()
 /*fecha o menu mobile após escolher a opção*/
 function menu_mobile_fechar()
 {
+      
 	    document.getElementById("menus").style.display = "none";
 }
